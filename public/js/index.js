399521d84efa20d844c8d522bb1bdf0682830962
Daoust <you@example.com>
2025-01-26 13:46:26 -0600

    installed express and nodemon and altered directories

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,10 @@
-"use strict";
+(function() {
+    "use strict"
 
-(() => {
 document.querySelector("#testButton").addEventListener("click", handleClick); 
 
 function handleClick() {
     console.log("Thank you for clicking.")
 }
-} )();
+
+} ());
