57def93d3cfd7886ebb76bba4b97e69aaa6f5a63
Daoust <you@example.com>
2025-02-12 21:03:15 -0600

    updated contact page and updated look of main page

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,4 @@
 (function() {
     "use strict"
 
-document.querySelector("#testButton").addEventListener("click", handleClick); 
-
-function handleClick() {
-    console.log("Thank you for clicking.")
-}
-
 } ());
