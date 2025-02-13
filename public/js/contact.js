d54f8952218621fa8542c190a7a64061ec708106
Daoust <you@example.com>
2025-02-12 22:40:07 -0600

    updated contact page

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,20 +1,35 @@
 (function() {
     "use strict"
 
+ let form = document.querySelector('#contact-form')
+
 document
 .querySelector("#contact-form-button") 
 .addEventListener("click", (event) => {
     event.preventDefault();
     event.stopPropagation();
-    console.log("You clicked the submit button.");
-    let name = document.querySelector("#name").value;
-    let email = document.querySelector("#mail").value;
-    let message = document.querySelector("#msg").value;
-    console.log("Name: " + name);
-    console.log("Email: " + email);
-    console.log("Message: " + message);
+    let formValid = true;
+    if (!form.checkValidity()) {
+        formValid = false;
+    }
+    form.classList.add('was-validated')
 
+if(formValid) {
+    sendTheEmail();
+}
+ 
 }); 
 
+function sendTheEmail(){
+    console.log("You clicked the submit button.");
+    let firstName = document.querySelector("#first-name").value;
+    let lastName = document.querySelector("#last-name").value;
+    let email = document.querySelector("#mail").value;
+    let message = document.querySelector("#msg").value;
+    console.log("First name: " + firstName);
+    console.log("Last name: " + lastName);
+    console.log("Email: " + email);
+    console.log("Message: " + message);
+}
 
-} )();
+} )()
