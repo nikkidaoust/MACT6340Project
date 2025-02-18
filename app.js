e6ac77a0081b797b7b595c428616889cefb9cdd7
Daoust <you@example.com>
2025-02-17 23:12:03 -0600

    Added nodemailer

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 import express from "express";
+import dotenv from "dotenv";
+import * as utils from "./utils/utils.js";
+dotenv.config();
+
 const app = express();
 const port = 3000;
 
+app.use(express.json());
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.send('Hello World!')
 });
 
+app.post("/mail", async (req, res) => {
+  await utils
+    .sendMessage(req.body.sub, req.body.txt)
+    .then(() => {
+      res.send({ result: "Submission Successful" });
+    })
+    .catch(() => {
+      res.send({ result: "failure" });
+    });
+});
+
 app.listen(port, () => {
+  console.log(process.env.SENSITIVE_INFO);
   console.log(`Example app listening on port ${port}`);
 });
--- public/js/contact.js
+++ public/js/contact.js
@@ -12,24 +12,39 @@ document
     if (!form.checkValidity()) {
         formValid = false;
     }
-    form.classList.add('was-validated')
-
-if(formValid) {
-    sendTheEmail();
+    form.classList.add('was-validated');
+    if(formValid) {
+          sendTheEmail();
 }
  
 }); 
 
 function sendTheEmail(){
     console.log("You clicked the submit button.");
-    let firstName = document.querySelector("#first-name").value;
-    let lastName = document.querySelector("#last-name").value;
-    let email = document.querySelector("#mail").value;
-    let message = document.querySelector("#msg").value;
-    console.log("First name: " + firstName);
-    console.log("Last name: " + lastName);
-    console.log("Email: " + email);
-    console.log("Message: " + message);
-}
-
-} )()
+    let obj = {
+        sub:"Someone submitted a contact form!",
+        text: `${document.querySelector("#first-name").value} 
+        ${document.querySelector("#last-name").value} 
+        sent you a message that reads ${document.querySelector("#msg").value}. 
+        Their email address is ${document.querySelector("#mail").value}`,
+    };
+   
+    fetch("/mail", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(obj),
+      })
+        .then((r) => r.json())
+        .then((response) => {
+          document.querySelector("#contact-form-button").innerHTML =
+            response.result;
+        })
+        .then(() => {
+          setTimeout(() => {
+            document.querySelector("#contact-form-button").innerHTML = "";
+          }, "5000");
+        });
+    }
+  })();
