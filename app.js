8d97a9828a2b1e82bc5321b5578d5bf88a708c75
Daoust <you@example.com>
2025-03-02 16:52:51 -0600

    moved html files to views folder and changed them to .ejs, fixed links referring to these files, added header and footer pages with shortcuts to all in each page, added an error page, added a project page for each individual project

--- app.js
+++ app.js
@@ -2,15 +2,36 @@ import express from "express";
 import dotenv from "dotenv";
 import * as utils from "./utils/utils.js";
 dotenv.config();
+let data = ["Project 1", "Project 2", "Project 3"];
 
 const app = express();
 const port = 3000;
-
+app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.send('Hello World!')
+  res.render("index.ejs");
+});
+
+app.get("/projects", (req, res) => {
+  res.render("projects.ejs", { projectArray: data});
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact.ejs");
+});
+
+app.get("/newProject", (req, res) => {
+  res.render("newProject.ejs");
+});
+
+app.get("/project/:id", (req, res) => {
+  let id = req.params.id;
+  if (id > data.length) {
+    throw new Error("No project with that ID");
+  }
+  res.render("project.ejs", {projectArray: data, which: id});
 });
 
 app.post("/mail", async (req, res) => {
@@ -22,6 +43,12 @@ app.post("/mail", async (req, res) => {
     .catch(() => {
       res.send({ result: "failure" });
     });
+});
+
+app.use((err,req, res, next) => {
+console.log(err);
+  res.render("error.ejs");
+
 });
 
 app.listen(port, () => {
