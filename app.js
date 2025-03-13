f2cd40f89e43a1588b8172ef528bcc00aa13ba10
Daoust <you@example.com>
2025-03-12 22:36:33 -0500

    added project cards, issue connecting to project list, MySQL2

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import * as utils from "./utils/utils.js";
 dotenv.config();
+import * as db from "./utils/database.js";
 let data = ["Project 1", "Project 2", "Project 3"];
+let projects = [];
 
 const app = express();
 const port = 3000;
@@ -10,20 +12,21 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("index.ejs");
+app.get("/", async (req, res, next) => {
+  await db
+    .connect()
+    .then(async () => {
+      // query the database for project records
+      projects = await db.getAllProjects();
+      console.log(projects);
+      let featuredRand = Math.floor(Math.random() * projects.length);
+      res.render("index.ejs", { featuredProject: projects[featuredRand] });
+    })
+    .catch(next);
 });
 
 app.get("/projects", (req, res) => {
-  res.render("projects.ejs", { projectArray: data});
-});
-
-app.get("/contact", (req, res) => {
-  res.render("contact.ejs");
-});
-
-app.get("/newProject", (req, res) => {
-  res.render("newProject.ejs");
+  res.render("projects.ejs", { projectArray: projects });
 });
 
 app.get("/project/:id", (req, res) => {
@@ -31,27 +34,35 @@ app.get("/project/:id", (req, res) => {
   if (id > data.length) {
     throw new Error("No project with that ID");
   }
-  res.render("project.ejs", {projectArray: data, which: id});
+  res.render("project.ejs", { project: projects[id - 1], which: id });
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact.ejs");
 });
 
 app.post("/mail", async (req, res) => {
   await utils
     .sendMessage(req.body.sub, req.body.txt)
     .then(() => {
-      res.send({ result: "Submission Successful" });
+      res.send({ result: "success" });
     })
     .catch(() => {
       res.send({ result: "failure" });
     });
 });
 
-app.use((err,req, res, next) => {
-console.log(err);
-  res.render("error.ejs");
-
+app.use(async (err, req, res, next) => {
+  console.log(err);
+  let msg;
+  msg = err.message;
+  if (msg != "No project with that ID") {
+    msg =
+      "There was an internal error. Apologies. We are working on cleaning up the mess.";
+  }
+  res.render("error.ejs", { msg: msg });
 });
 
 app.listen(port, () => {
-  console.log(process.env.SENSITIVE_INFO);
   console.log(`Example app listening on port ${port}`);
 });
