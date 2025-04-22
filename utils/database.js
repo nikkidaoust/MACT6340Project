674b3aaed1fde53afee8bfc837949a5f02e0ce22
Daoust <you@example.com>
2025-04-21 21:30:39 -0500

    final push prior to deployment

--- utils/database.js
+++ utils/database.js
@@ -19,13 +19,13 @@ export async function connect() {
   pool = mysql
     .createPool
     (
-      // cString  //digital ocean sql server
-      {
-        host: process.env.MYSQL_HOST,
-        user: process.env.MYSQL_USER,
-        password: process.env.MYSQL_PASSWORD,
-        database: process.env.MYSQL_DATABASE,
-      }
+      cString  //digital ocean sql server
+      // {
+      //   host: process.env.MYSQL_HOST,
+      //   user: process.env.MYSQL_USER,
+      //   password: process.env.MYSQL_PASSWORD,
+      //   database: process.env.MYSQL_DATABASE,
+      // }
     )
     .promise();
 }
