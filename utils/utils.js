48e1aff9fc170f24e12d6786c57cbfc4c8f12fb3
Daoust <you@example.com>
2025-04-23 23:24:41 -0500

    final project

--- utils/utils.js
+++ utils/utils.js
@@ -2,12 +2,12 @@ import nodemailer from "nodemailer";
 
 export async function sendMessage(sub, txt) {
   let transporter = nodemailer.createTransport({
-    host: process.env.MAIL_HOST,
+    host: process.env.GMAIL_HOST,
     port: process.env.MAIL_PORT,
     secure: process.env.MAIL_SECURE,
     auth: {
-      user: process.env.MAIL_USERNAME,
-      pass: process.env.MAIL_PASSWORD,
+      user: process.env.GMAIL_USERNAME,
+      pass: process.env.GMAIL_PASSWORD,
     },
     requireTLS: process.env.MAIL_TLS,
   });
